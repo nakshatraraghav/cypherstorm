defb9472c8a605f5f7d16830d01907c3346ef631
nakshatra <combatrickshaw@gmail.com>
2024-11-11 18:57:33 +0530

    ran go mod tidy

--- cmd/root.go
+++ cmd/root.go
@@ -2,8 +2,13 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 	"os"
 
+	"github.com/nakshatraraghav/cypherstorm/internal/archiver"
+	"github.com/nakshatraraghav/cypherstorm/internal/compression"
+	"github.com/nakshatraraghav/cypherstorm/internal/encryption"
+	"github.com/nakshatraraghav/cypherstorm/internal/keyman"
 	"github.com/spf13/cobra"
 )
 
@@ -13,7 +18,65 @@ var rootCmd = &cobra.Command{
 	Long:    "CypherStorm is a cryptographic suite of tools for compressing, encrypting, and hashing files or folders with customizable algorithms, providing flexible, high-security file management",
 	Aliases: []string{"cypher", "cstorm"},
 	Run: func(cmd *cobra.Command, args []string) {
-		cmd.Help()
+		err := archiver.CreateTarArchive("/home/combatrickshaw/Downloads/test", "/home/combatrickshaw/Downloads/archive.tar")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		cmp := compression.NewGzipCompressor()
+
+		archive, err := os.Open("/home/combatrickshaw/Downloads/archive.tar")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		compressed, err := os.Create("/home/combatrickshaw/Downloads/archive.tar.cmp")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = cmp.Compress(archive, compressed)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		km := keyman.NewKeyManager(32, 16)
+		key, err := km.DeriveKeyFromPassword("helloworld")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		enc := encryption.NewAesGcmEncryptor(key)
+
+		compressedArchive, err := os.Open("/home/combatrickshaw/Downloads/archive.tar.cmp")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		encrypted, err := os.Create("/home/combatrickshaw/Downloads/archive.tar.cmp.enc")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = enc.Encrypt(compressedArchive, encrypted)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		encryptedFile, err := os.Open("/home/combatrickshaw/Downloads/archive.tar.cmp.enc")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		decrypted, err := os.Create("/home/combatrickshaw/Downloads/decrypted.tar.cmp")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = enc.Decrypt(encryptedFile, decrypted)
+		if err != nil {
+			log.Fatal(err)
+		}
 	},
 }
 
--- go.mod
+++ go.mod
@@ -8,11 +8,11 @@ require (
 	github.com/pierrec/lz4/v4 v4.1.21
 	github.com/spf13/cobra v1.8.1
 	github.com/ulikunitz/xz v0.5.12
+	golang.org/x/crypto v0.29.0
 )
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/crypto v0.29.0 // indirect
 	golang.org/x/sys v0.27.0 // indirect
 )
