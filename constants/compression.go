a50bdd1f7bc0e2e1dc3789f4f9adf4a3ef98fb55
nakshatra <combatrickshaw@gmail.com>
2024-11-12 22:37:32 +0530

    algorithm constants

---
+++ constants/compression.go
@@ -0,0 +1,9 @@
+package constants
+
+var (
+	BZIP_COMPRESSION = "bzip"
+	GZIP_COMPRESSION = "gzip"
+	LZ4_COMPRESSION  = "lz4"
+	LZMA_COMPRESSION = "lzma"
+	ZSTD_COMPRESSION = "zstd"
+)
---
+++ constants/encryption.go
@@ -0,0 +1,6 @@
+package constants
+
+const (
+	AES_256_GCM        string = "aes-256-gcm"
+	XCHACHA20_POLY1305 string = "xchacha20poly1305"
+)
